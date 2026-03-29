d47be7d34095feee2ef5b5510cecd6440281fe98
Naman <pandyanaman2@gmail.com>
2026-03-29 17:24:48 +0530

    Re-added app.js without secrets

---
+++ server/app.js
@@ -0,0 +1,39 @@
+import dotenv from "dotenv"
+dotenv.config()
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import directoryRoutes from "./routes/directoryRoutes.js";
+import fileRoutes from "./routes/fileRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+import authRoutes from "./routes/authRoutes.js";
+import checkAuth from "./middlewares/auth.js";
+import { connectDB } from "./config/db.js";
+
+const mySecretKey = process.env.SECRET_KEY
+
+await connectDB();
+
+const app = express();
+app.use(cookieParser(mySecretKey));
+app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+
+app.use("/directory", checkAuth, directoryRoutes);
+app.use("/file", checkAuth, fileRoutes);
+app.use("/user", userRoutes);
+app.use("/auth", authRoutes);
+
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.status(err.status || 500).json({ error: "Something went wrong!" });
+});
+
+app.listen(4000, () => {
+  console.log(`Server Started`);
+});
